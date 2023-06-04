23014a8ff82dd289b2976599a359df001855432e
Júlio César Batista <julio.batista@outlook.com>
2023-06-04 16:14:25 -0300

    handle 401

--- src/http_client.py
+++ src/http_client.py
@@ -1,4 +1,4 @@
-from typing import Protocol
+from typing import Protocol, Dict, Callable
 import requests
 from os import path
 
@@ -8,14 +8,49 @@ class HttpClient(Protocol):
         """Performs a HTTP GET request and returns the parsed result"""
 
 
+class UnauthorizedError(ValueError):
+    apikey: str
+    response_text: str
+
+    def __init__(self, apikey: str, response_text: str):
+        self.apikey = apikey
+        self.response_text = response_text
+
+
+response_handler = Callable[[dict], requests.Response]
+
+
 class DefaultHttpClient:    
     _apikey: str
     _base_url: str = "https://the-one-api.dev/v2/"
+    _handlers: Dict[int, response_handler]
 
     def __init__(self, apikey: str):
+        if not apikey or not apikey.strip():
+            raise ValueError("invalid apikey")
+
         self._apikey = apikey
+        self._handlers = dict()
+        self.with_response_handler(200, self._handle_200)
+        self.with_response_handler(401, self._handle_401)
 
     def get(self, url: str) -> dict:
+        return self.handle_response(self.make_request(url))
+    
+    def make_request(self, url: str) -> requests.Response:
         return requests.get(path.join(self._base_url, url), headers={
             "Authorization": f"Bearer {self._apikey}"
-        }).json()
+        })
+
+    def with_response_handler(self, status_code: int, handler: response_handler) -> None:
+        self._handlers[status_code] = handler
+
+    def handle_response(self, response: requests.Response) -> dict:
+        handler = self._handlers.get(response.status_code)
+        return handler(response)
+    
+    def _handle_200(self, response: requests.Response) -> dict:
+        return response.json()
+    
+    def _handle_401(self, response: requests.Response) -> dict:
+        raise UnauthorizedError(f"{self._apikey[:5]}...", response.text)
--- src/test_http_client.py
+++ src/test_http_client.py
@@ -1,8 +1,8 @@
-from http_client import DefaultHttpClient
+from http_client import DefaultHttpClient, UnauthorizedError
 import json
 from test_client import make_api_response
 from test_movie import LOTR_1_FAKE_RESPONSE
-
+import pytest
 
 def test_get(requests_mock):
     response = make_api_response(LOTR_1_FAKE_RESPONSE)
@@ -13,3 +13,17 @@ def test_get(requests_mock):
 
     assert result == response
     assert requests_mock.last_request.headers["Authorization"] == "Bearer apikey-123"
+
+def test_invalid_apikey():
+    with pytest.raises(ValueError):
+        DefaultHttpClient("")
+
+def test_handle_401(requests_mock):
+    requests_mock.get("https://the-one-api.dev/v2/movie/123", status_code=401, text="response 401")
+    http = DefaultHttpClient("bad-api-key")
+
+    with pytest.raises(UnauthorizedError) as e:
+        http.get("movie/123")
+
+        assert e.apikey == "bad-a..."
+        assert e.response_text == "response 401"
